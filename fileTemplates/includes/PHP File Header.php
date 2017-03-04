5d1264591f68d7ba5ffdf5147da23d3e994b1e29
christophherr <info@christophherr.com>
2017-03-03 19:08:41 -0500

    PS-171.3691.14 <Christoph@Vostro3350 Create baseRefactoring.xml, IntelliLang.xml, ide.general.xml, git.xml, project.default.xml, diff.xml, Default copy.xml, other.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, ui.lnf.xml, SQL Script.sql, filetypes.xml, php.xml, keymap.xml, code.style.schemes.xml, plugin_ui.xml, colors.scheme.xml, Monokai _mine_.icls, Default.xml, hg.xml, laf.xml, Seti_JB.icls, Monokai_Sublime.icls, PHP File Header.php, Default _1_.xml, web-browsers.xml, gitlab-project-settings.xml, Vuejs.xml, databaseSettings.xml, Material Theme - Darker copy.icls, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,9 @@
+/**
+ * Description
+ * 
+ * @package     ${NAMESPACE}
+ * @since       1.0.0
+ * @author      Christoph Herr
+ * @link        https://www.christophherr.com
+ * @license     GNU General Public License 2+
+ */
